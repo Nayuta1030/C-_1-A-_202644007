f4fbc69af3d3131e30a630be80e5e9bca9446a66
Nayuta1030 <ghostbellehauntedmansion@gmail.com>
2026-09-15 15:55:28 +0900

    Add files via upload

---
+++ C3/c3_for.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for (int i=5; i>=0; i--) {
+        for (int j=1; j <=i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
